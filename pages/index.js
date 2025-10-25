8bbc518649dfb0186778aa7ea344e982924fe334
Pedro Bozan <60632034+PedroBozan@users.noreply.github.com>
2025-10-25 23:35:16 +0000

    Altera a frase da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O mundo é grande demais pra você ficar apenas dentro de casa!</h1>
+    return <h1>O mundo é grande demais pra você ficar apenas dentro de casa!!!</h1>
 }
 
 export default Home;
